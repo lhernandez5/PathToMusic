103fab6057885dd79460465bd3e12a72e08c2c31
Luz_Hernandez <hernandezluz18@gmail.com>
2018-06-20 23:11:58 -0400

    firebase functions modified

--- youtubeapi.js
+++ youtubeapi.js
@@ -1,22 +1,51 @@
 // YoutubeApiBranch
 $(document).ready(function() {
   console.log("ready!");
+  //FIREBASE SAVED RECENT SEARCHED THIS SECTION
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDYFaKwZCcC6RsKkC6Oyd4Le3P-A_pKlNM",
+    authDomain: "path2muzik-1529345243230.firebaseapp.com",
+    databaseURL: "https://path2muzik-1529345243230.firebaseio.com",
+    projectId: "path2muzik-1529345243230",
+    storageBucket: "path2muzik-1529345243230.appspot.com",
+    messagingSenderId: "40158218059"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var bandName = "";
+  
+  $("#find-artist").on("click", function(event) {
+    event.preventDefault();
+    bandName = $("#artist-input").val().trim();
+    console.log(bandName);
+
+    //PUSH CLICK DATA TO FIREBASE
+    database.ref().push({
+      band: bandName
+    });
+  });
+  database.ref().on("child_added",function(childSnapshot) {console.log(childSnapshot.val());
+
+      var recentBandName = childSnapshot.val().band;
+
+      $("#recent-searches").prepend("<ul><li>" + recentBandName + "</li></ul>");
+    },
+    function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    }
+  );
 
   //YOUTUBE BAND QUERY THIS SECTION
   $("#find-artist").on("click", function(event) {
     event.preventDefault();
-    var keyword = $("#artist-input")
-      .val()
-      .trim()
-      .toLowerCase();
+    var keyword = $("#artist-input").val().trim().toLowerCase();
     var apikey = "AIzaSyCYpIVDYnDUF_X8RT4v_0DYoWuIxjnPMMI";
     // var keyword = "pink"; THIS LINE FOR TESTING BEFORE INPUT RECEIVED FROM TEAM
     var youtube = "https://www.youtube.com/watch?v=";
     var outerQueryURL =
-      "https://www.googleapis.com/youtube/v3/search?q=" +
-      keyword +
-      "&part=snippet&type=video&maxResults=6&results=6&order=viewCount&key=" +
-      apikey;
+      "https://www.googleapis.com/youtube/v3/search?q=" + keyword +"&part=snippet&type=video&maxResults=6&results=6&order=viewCount&key=" + apikey;
     $.ajax({
       url: outerQueryURL,
       method: "GET"
@@ -31,56 +60,11 @@ $(document).ready(function() {
 
         console.log(youtube + videoId);
         console.log(videoTitle);
-        //   $("#songLink").append("<a href=" + videoLink + ">" + videoTitle + "</a>");
         $("#tbody2").append(
           "<tr><td><a href=" + videoLink + ">" + videoTitle + "</a></td></tr>"
         );
       }
-      whenBandClicked();
     });
-  });
-
-  //FIREBASE SAVED RECENT SEARCHED THIS SECTION
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDYFaKwZCcC6RsKkC6Oyd4Le3P-A_pKlNM",
-    authDomain: "path2muzik-1529345243230.firebaseapp.com",
-    databaseURL: "https://path2muzik-1529345243230.firebaseio.com",
-    projectId: "path2muzik-1529345243230",
-    storageBucket: "path2muzik-1529345243230.appspot.com",
-    messagingSenderId: "40158218059"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-
-  // $("#submitBand").on("click ",
-  function whenBandClicked() {
-    event.preventDefault();
-    var bandName = $("#artist-input")
-      .val()
-      .trim();
-    console.log(bandName);
-
-    var recentSearch = {
-      bandName: bandName
-    };
-
-    //PUSH CLICK DATA TO FIREBASE
-    database.ref().push(recentSearch);
-    console.log(recentSearch.bandName);
-
-    //CLEAR FORM INPUTS
-    $("#artist-input").val("");
-    //
-  }
-
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
-
-    var recentBandName = childSnapshot.val().bandName;
-
-    $("#recent-searches").prepend("<ul><li>" + recentBandName + "</li></ul>");
   });
 
   $("#find-artist").on("click", function(event) {
@@ -88,19 +72,16 @@ $(document).ready(function() {
     $("h4").hide();
     var keyword = $("#artist-input").val().trim();
     if (keyword !== "") {
-      var url =
-        "https://app.ticketmaster.com/discovery/v2/events.json?keyword=";
+      var url ="https://app.ticketmaster.com/discovery/v2/events.json?keyword=";
       console.log(keyword);
       $("#artist-input").val("");
-      var key =
-        "&countryCode=US&sort=date,asc&apikey=wzw1aN0lKG2c96suxa2buGqcNWjIodvq";
+      var key ="&countryCode=US&sort=date,asc&apikey=wzw1aN0lKG2c96suxa2buGqcNWjIodvq";
       var queryURL = url + keyword + key;
 
       $.ajax({
         url: queryURL,
         method: "GET"
-      })
-        .then(function(response) {
+      }).then(function(response) {
           $("#tbody1").empty();
           console.log(response);
           var h = response._embedded.events;
@@ -123,69 +104,67 @@ $(document).ready(function() {
             tRow.append(titleTd, date, ticketUrl, imageOnScreen);
             tBody.append(tRow);
           }
-        })
-        .catch(err => {
+        }).catch(err => {
           console.log(err);
           $("#artist-view").html("Sorry no results.");
         });
-    }else{
-        $("h4").show();
-    $("h4").html("Invalid. No response for artist.");
-}
+    } else {
+      $("h4").show();
+      $("h4").html("Invalid. No response for artist.");
+    }
   });
 
   $("#find-city").on("click", function(event) {
     event.preventDefault();
     $("h4").hide();
-    var state = $("#state-input").val().trim();
-    var city = $("#city-input").val().trim();
+    var state = $("#state-input")
+      .val()
+      .trim();
+    var city = $("#city-input")
+      .val()
+      .trim();
     var letters = /^[A-Za-z]+$/;
-    if ((state.length === 2) && (state !== "") && (state.match(letters)) &&((city !== "")&&(city.match(letters)))) {
-    var url = "https://app.ticketmaster.com/discovery/v2/events.json?";
-    var country = "&countryCode=US&stateCode=";
-    var key = "&sort=date,asc&apikey=wzw1aN0lKG2c96suxa2buGqcNWjIodvq";
-    var queryURL = url + country + state + "&city=" + city + key;
+    if (state.length === 2 && state !== "" && state.match(letters) &&(city !== "" && city.match(letters))) {
+      var url = "https://app.ticketmaster.com/discovery/v2/events.json?";
+      var country = "&countryCode=US&stateCode=";
+      var key = "&sort=date,asc&apikey=wzw1aN0lKG2c96suxa2buGqcNWjIodvq";
+      var queryURL = url + country + state + "&city=" + city + key;
 
-    console.log(queryURL);
-    // $("#state-input").val("");
-    // $("#city-input").val("");
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        $("tbody").empty();
-        console.log(response);
-        var h = response._embedded.events;
-        for (var i = 0; i < h.length; i++) {
-          var tBody = $("#tbody1");
-          var tRow = $("<tr>");
-          var titleTd = $("<td>").text(response._embedded.events[i].name);
-          var date = $("<td>").text(
-            response._embedded.events[i].dates.start.localDate
-          );
-          var url = $("<td>").text(response._embedded.events[i].url);
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          $("tbody").empty();
+          console.log(response);
+          var h = response._embedded.events;
+          for (var i = 0; i < h.length; i++) {
+            var tBody = $("#tbody1");
+            var tRow = $("<tr>");
+            var titleTd = $("<td>").text(response._embedded.events[i].name);
+            var date = $("<td>").text(
+              response._embedded.events[i].dates.start.localDate
+            );
+            var url = $("<td>").text(response._embedded.events[i].url);
 
-          var eventUrl = response._embedded.events[i].url;
-          var pfive = $("<a>")
-            .attr("href", eventUrl)
-            .append(eventUrl);
-          var p = $("<td>").html(pfive);
-          var imgURL = response._embedded.events[i].images[0].url;
-          var image = $("<img style='width: 300px'>").attr("src", imgURL);
-          var imageOnScreen = $("<td>").html(image);
-          image.addClass("artistImg");
-          tRow.append(titleTd, date, p, imageOnScreen);
-          tBody.append(tRow);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        $("#artist-view").html("Sorry no results.");
-      });
-    }else{
-        $("h4").show();
-        $("h4").html("Invalid. You need to input a city and a state.");
+            var eventUrl = response._embedded.events[i].url;
+            var pfive = $("<a>")
+              .attr("href", eventUrl)
+              .append(eventUrl);
+            var p = $("<td>").html(pfive);
+            var imgURL = response._embedded.events[i].images[0].url;
+            var image = $("<img style='width: 300px'>").attr("src", imgURL);
+            var imageOnScreen = $("<td>").html(image);
+            image.addClass("artistImg");
+            tRow.append(titleTd, date, p, imageOnScreen);
+            tBody.append(tRow);
+          }
+        }).catch(err => {
+          console.log(err);
+          $("#artist-view").html("Sorry no results.");
+        });
+    } else {
+      $("h4").show();
+      $("h4").html("Invalid. You need to input a city and a state.");
     }
   });
 });
